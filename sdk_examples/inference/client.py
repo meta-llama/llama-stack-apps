ae9d24632ca157d3ef51d9b8aaaf7a3d60d23eb3
Xi Yan <xiyan@meta.com>
2024-09-11 15:41:07 -0700

    update inference client

--- sdk_examples/inference/client.py
+++ sdk_examples/inference/client.py
@@ -2,30 +2,29 @@ import fire
 from llama_stack import LlamaStack
 from llama_stack.types import UserMessage
 
-def main(host: str, port: int):
+
+def main(host: str, port: int, stream: bool = True):
     client = LlamaStack(
         base_url=f"http://{host}:{port}",
     )
 
-    response = client.inference.chat_completion(
-        request={
-            "messages": [
-                UserMessage(content="hello world, troll me in two-paragraphs about 42", role="user"),
-            ],
-            "model": "Meta-Llama3.1-8B-Instruct",
-            "stream": False,
-        },
+    response_iterator = client.inference.chat_completion(
+        messages=[
+            UserMessage(
+                content="hello world, troll me in two-paragraphs about 42", role="user"
+            ),
+        ],
+        model="Meta-Llama3.1-8B-Instruct",
+        stream=stream,
     )
 
-    print(response)
-    # TODO (xiyan). This does not work with current server, need to fix
-    # response = client.inference.chat_completion(
-    #     messages=[
-    #         UserMessage(content="hello world, troll me in two-paragraphs about 42", role="user"),
-    #     ],
-    #     model="Meta-Llama3.1-8B-Instruct",
-    #     stream=True,
-    # )
+    if not stream:
+        print(response_iterator)
+        return
+
+    for line in response_iterator:
+        print(line)
+
 
 if __name__ == "__main__":
     fire.Fire(main)
