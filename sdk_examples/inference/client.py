1d40c26b2acf995ef1d783d21bdd59aeb94f22f9
Xi Yan <xiyan@meta.com>
2024-09-11 17:28:55 -0700

    inference example

--- sdk_examples/inference/client.py
+++ sdk_examples/inference/client.py
@@ -1,14 +1,60 @@
+import asyncio
+
 import fire
+
 from llama_stack import LlamaStack
-from llama_stack.types import UserMessage
+from llama_stack.types import (
+    ChatCompletionResponse,
+    ChatCompletionStreamChunk,
+    UserMessage,
+)
+from termcolor import cprint
 
 
-def main(host: str, port: int, stream: bool = True):
+class LogEvent:
+    def __init__(
+        self,
+        content: str = "",
+        end: str = "\n",
+        color="white",
+    ):
+        self.content = content
+        self.color = color
+        self.end = "\n" if end is None else end
+
+    def print(self, flush=True):
+        cprint(f"{self.content}", color=self.color, end=self.end, flush=flush)
+
+
+class EventLogger:
+    async def log(self, event_generator):
+        for chunk in event_generator:
+            if isinstance(chunk, ChatCompletionStreamChunk):
+                event = chunk.event
+                if event.event_type == "start":
+                    yield LogEvent("Assistant> ", color="cyan", end="")
+                elif event.event_type == "progress":
+                    yield LogEvent(event.delta, color="yellow", end="")
+                elif event.event_type == "complete":
+                    yield LogEvent("")
+            elif isinstance(chunk, ChatCompletionResponse):
+                yield LogEvent("Assistant> ", color="cyan", end="")
+                yield LogEvent(chunk.completion_message.content, color="yellow")
+            else:
+                yield LogEvent("Assistant> ", color="cyan", end="")
+                yield LogEvent(chunk, color="yellow")
+
+
+async def run_main(host: str, port: int, stream: bool = True):
     client = LlamaStack(
         base_url=f"http://{host}:{port}",
     )
 
-    response_iterator = client.inference.chat_completion(
+    message = UserMessage(
+        content="hello world, troll me in two-paragraphs about 42", role="user"
+    )
+    cprint(f"User>{message.content}", "green")
+    iterator = client.inference.chat_completion(
         messages=[
             UserMessage(
                 content="hello world, troll me in two-paragraphs about 42", role="user"
@@ -18,12 +64,12 @@ def main(host: str, port: int, stream: bool = True):
         stream=stream,
     )
 
-    if not stream:
-        print(response_iterator)
-        return
+    async for log in EventLogger().log(iterator):
+        log.print()
 
-    for line in response_iterator:
-        print(line)
+
+def main(host: str, port: int, stream: bool = True):
+    asyncio.run(run_main(host, port, stream))
 
 
 if __name__ == "__main__":
