0c5c93a149fca403f2115de1511f599474283b80
Xi Yan <xiyan@meta.com>
2024-09-19 11:01:19 -0700

    update file upload for memory client sdk

--- sdk_examples/memory/client.py
+++ sdk_examples/memory/client.py
@@ -1,11 +1,30 @@
 import asyncio
+import base64
 import json
+import mimetypes
+import os
+from pathlib import Path
 
 import fire
 
 from llama_stack import LlamaStack
 from llama_stack.types.memory_bank_insert_params import Document
 from termcolor import cprint
+
+
+def data_url_from_file(file_path: str) -> str:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+
+    base64_content = base64.b64encode(file_content).decode("utf-8")
+    mime_type, _ = mimetypes.guess_type(file_path)
+
+    data_url = f"data:{mime_type};base64,{base64_content}"
+
+    return data_url
 
 
 async def run_main(host: str, port: int, stream: bool = True):
@@ -52,6 +71,16 @@ async def run_main(host: str, port: int, stream: bool = True):
         for i, url in enumerate(urls)
     ]
 
+    this_dir = os.path.dirname(__file__)
+    files = [Path(this_dir).parent.parent / "CONTRIBUTING.md"]
+    documents += [
+        Document(
+            document_id=f"num-{i}",
+            content=data_url_from_file(path),
+        )
+        for i, path in enumerate(files)
+    ]
+
     # insert some documents
     client.memory_banks.insert(
         bank_id=bank["bank_id"],
@@ -73,6 +102,16 @@ async def run_main(host: str, port: int, stream: bool = True):
         bank_id=bank["bank_id"],
         query=[
             "Tell me more about llama3 and torchtune",
+        ],
+    )
+    for chunk, score in zip(response.chunks, response.scores):
+        print(f"Score: {score}")
+        print(f"Chunk:\n========\n{chunk}\n========\n")
+
+    response = client.memory_banks.query(
+        bank_id=bank["bank_id"],
+        query=[
+            "Tell me more about llama models",
         ],
     )
     for chunk, score in zip(response.chunks, response.scores):
