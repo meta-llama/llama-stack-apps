cb8d4cbb8efa11bbed8aae32d05304ec32a9014a
Justin Lee <justinai@fb.com>
2024-11-11 18:41:53 -0800

    removed unnecessary files

--- examples/agents/inflation.py
+++ examples/agents/inflation.py
@@ -4,59 +4,87 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
-
 import asyncio
+import os
 
 import fire
 
-from llama_stack_client.types import Attachment, SamplingParams, UserMessage
-from llama_stack_client.types.agent_create_params import *  # noqa: F403
-from common.client_utils import *  # noqa: F403
-from examples.agents.multi_turn import execute_turns, prompt_to_turn
-from termcolor import cprint
+from llama_stack_client import LlamaStackClient
+from llama_stack_client.lib.agents.agent import Agent
+from llama_stack_client.lib.agents.event_logger import EventLogger
+from llama_stack_client.types import Attachment
+from llama_stack_client.types.agent_create_params import AgentConfig
 
 
 async def run_main(host: str, port: int, disable_safety: bool = False):
-    api_keys = load_api_keys_from_env()
-    tool_definitions = [
-        search_tool_defn(api_keys),
-        # Adding code_interpreter enables file analysis
-        AgentConfigToolCodeInterpreterToolDefinition(type="code_interpreter"),
+    client = LlamaStackClient(
+        base_url=f"http://{host}:{port}",
+    )
+
+    input_shields = [] if disable_safety else ["llama_guard"]
+    output_shields = [] if disable_safety else ["llama_guard"]
+
+    agent_config = AgentConfig(
+        model="Llama3.1-8B-Instruct",
+        instructions="You are a helpful assistant",
+        sampling_params={
+            "strategy": "greedy",
+            "temperature": 1.0,
+            "top_p": 0.9,
+        },
+        tools=[
+            {
+                "type": "brave_search",
+                "engine": "brave",
+                "api_key": os.getenv("BRAVE_SEARCH_API_KEY"),
+            },
+            {
+                "type": "code_interpreter",
+            },
+        ],
+        tool_choice="required",
+        tool_prompt_format="json",
+        input_shields=input_shields,
+        output_shields=output_shields,
+        enable_session_persistence=False,
+    )
+
+    agent = Agent(client, agent_config)
+    session_id = agent.create_session("test-session")
+    print(f"Created session_id={session_id} for Agent({agent.agent_id})")
+
+    user_prompts = [
+        (
+            "Here is a csv, can you describe it ?",
+            [
+                Attachment(
+                    content="https://raw.githubusercontent.com/meta-llama/llama-stack-apps/main/examples/resources/inflation.csv",
+                    mime_type="test/csv",
+                )
+            ],
+        ),
+        ("Which year ended with the highest inflation ?", None),
+        (
+            "What macro economic situations that led to such high inflation in that period?",
+            None,
+        ),
+        ("Plot average yearly inflation as a time series", None),
     ]
 
-    agent_config = await make_agent_config_with_custom_tools(
-        disable_safety=disable_safety,
-        tool_config=QuickToolConfig(
-            tool_definitions=tool_definitions,
-            custom_tools=[],
-            attachment_behavior="code_interpreter",
-        ),
-    )
+    for prompt in user_prompts:
+        response = agent.create_turn(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt[0],
+                }
+            ],
+            attachments=prompt[1],
+            session_id=session_id,
+        )
 
-    await execute_turns(
-        agent_config=agent_config,
-        custom_tools=[],
-        turn_inputs=[
-            # prompt_to_turn(
-            #     "Here is a csv, can you describe it ?",
-            #     attachments=[
-            #         Attachment(
-            #             content="https://raw.githubusercontent.com/meta-llama/llama-stack-apps/main/examples/resources/inflation.csv",
-            #             mime_type="text/csv",
-            #         ),
-            #     ],
-            # ),
-            prompt_to_turn("Which year ended with the highest inflation ?"),
-            prompt_to_turn(
-                "What macro economic situations that led to such high inflation in that period?"
-            ),
-            prompt_to_turn("Plot average yearly inflation as a time series"),
-        ],
-        host=host,
-        port=port,
-    )
+        async for log in EventLogger().log(response):
+            log.print()
 
 
 def main(host: str, port: int, disable_safety: bool = False):
