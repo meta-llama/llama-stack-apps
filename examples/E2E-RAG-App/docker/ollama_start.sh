098453597066fb3f6c2d8bf21ef53c3f0fdb5aa2
Kai Wu <kaiwu@meta.com>
2024-11-18 11:44:57 -0800

    add how to run

--- examples/E2E-RAG-App/docker/ollama_start.sh
+++ examples/E2E-RAG-App/docker/ollama_start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "-------------start to serve------------"
-/usr/bin/ollama serve > /dev/null 2>&1
-sleep 10 
+/usr/bin/ollama serve&
 echo "pulling ollama model: $MODEL_NAME"
 /usr/bin/ollama pull $MODEL_NAME
 echo "Running ollama model: $MODEL_NAME"
-/usr/bin/ollama run $MODEL_NAME 
+/usr/bin/ollama run $MODEL_NAME
 while :; do sleep 2073600; done
