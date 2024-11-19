208c76286c55d0703b6392a6a5a4f0cf88ce45f5
Kai Wu <kaiwu@meta.com>
2024-11-19 15:32:44 -0800

    gradio UI working but slow

--- examples/E2E-RAG-App/docker/llama_stack_start.sh
+++ examples/E2E-RAG-App/docker/llama_stack_start.sh
@@ -8,14 +8,13 @@ sleep 45
 #pip install tiktoken
 #pip install --upgrade --no-deps --force-reinstall --index-url https://test.pypi.org/simple/ llama_stack==0.0.53rc1
 pip install gradio
-echo "Installing llama-stack-client"
+# echo "Installing llama-stack-client"
 pip install distro
-pip install --index-url https://test.pypi.org/simple/ llama_stack_client==0.0.53rc2
+pip install --index-url https://test.pypi.org/simple/ llama_stack_client==0.0.53rc5
 #pip install git+https://github.com/meta-llama/llama-stack-client-python.git@f5a2391241eac03eea356b206469081688277d23
 echo "starting the llama-stack server"
 python -m llama_stack.distribution.server.server --yaml-config /root/my-run.yaml --disable-ipv6&
-sleep 3600000000
-
+sleep 36000000
 echo "running the RAG app"
-#python /root/E2E-RAG-App/gradio_interface.py
+python /root/E2E-RAG-App/gradio_interface.py
 #python /root/E2E-RAG-App/ollama_main.py localhost 5000
