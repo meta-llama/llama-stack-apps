0e7bc691f1d9afc874645f11dbe6252833fb08b5
Kai Wu <kaiwu@meta.com>
2024-11-18 17:39:16 -0800

    fix llama-stack version

--- examples/E2E-RAG-App/docker/llama_stack_start.sh
+++ examples/E2E-RAG-App/docker/llama_stack_start.sh
@@ -1,13 +1,15 @@
 
 sleep 60
 echo "starting to install llama-stack"
-apt-get update
-apt-get install -y git
+#apt-get update
+#apt-get install -y git
 #pip install /root/llama-stack
-pip install git+https://github.com/meta-llama/llama-stack.git@2edfda97e9659155074269fc3b7e66d9bb2c57d4
+#pip install git+https://github.com/meta-llama/llama-stack.git@2edfda97e9659155074269fc3b7e66d9bb2c57d4
 #pip install tiktoken
+pip install --upgrade --no-deps --force-reinstall --index-url https://test.pypi.org/simple/ llama_stack==0.0.53rc1
 pip install gradio
 echo "Installing llama-stack-client"
+pip install distro
 pip install --index-url https://test.pypi.org/simple/ llama_stack_client==0.0.53rc2
 #pip install git+https://github.com/meta-llama/llama-stack-client-python.git@f5a2391241eac03eea356b206469081688277d23
 echo "starting the llama-stack server"
