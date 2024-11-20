8f240edeac967570499d62d99c7b21fb579fb9c4
Kai Wu <kaiwu@meta.com>
2024-11-20 13:38:39 -0800

    kind of working

--- examples/E2E-RAG-App/docker/llama_stack_start.sh
+++ examples/E2E-RAG-App/docker/llama_stack_start.sh
@@ -1,8 +1,12 @@
 
 sleep 45
+
 echo "-----starting to llama-stack docker now---------"
 
 pip install gradio
+pip install docling
+
+python /root/E2E-RAG-App/01_ingest_files.py --input_dir /root/rag_data/
 
 echo "starting the llama-stack server"
 
@@ -10,4 +14,5 @@ python -m llama_stack.distribution.server.server --yaml-config /root/my-run.yaml
 
 sleep 30
 echo "---------running the RAG app--------------"
+
 python /root/E2E-RAG-App/gradio_interface.py
