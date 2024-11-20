fa0cf90bbc7de5fe572fdc8b07e857e968a5db33
Kai Wu <kaiwu@meta.com>
2024-11-19 20:31:32 -0800

    add how to run readme

--- examples/E2E-RAG-App/docker/llama_stack_start.sh
+++ examples/E2E-RAG-App/docker/llama_stack_start.sh
@@ -1,12 +1,13 @@
 
 sleep 45
-#echo "starting to install llama-stack"
+echo "-----starting to llama-stack docker now---------"
 
 pip install gradio
 
 echo "starting the llama-stack server"
+
 python -m llama_stack.distribution.server.server --yaml-config /root/my-run.yaml --disable-ipv6&
-sleep 36000000
-echo "running the RAG app"
+
+sleep 30
+echo "---------running the RAG app--------------"
 python /root/E2E-RAG-App/gradio_interface.py
-#python /root/E2E-RAG-App/ollama_main.py localhost 5000
