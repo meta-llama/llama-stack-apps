7c68366dbbf44539e92c2e555083afe801bef2a4
Xi Yan <xiyan@meta.com>
2024-12-15 22:35:15 -0800

    simulated user done

--- tau-benchmark/main.py
+++ tau-benchmark/main.py
@@ -10,16 +10,27 @@ from .agents import get_retail_agent
 from .agents.retail.tools import ALL_TOOLS
 from .base_env import BaseEnv
 from .tasks.retail.tasks_dev import TASKS_DEV
-from .users.users import HumanUser
+
+from .users.users import HumanUser, SimulatedUser
+
+MAX_STEPS = 30
+STOP_TOKEN = "###STOP###"
 
 
-def main():
-    max_steps = 30
+def run_single_task(task_idx: int = 0, user_type: str = "simulated"):
+    max_steps = MAX_STEPS
     agent = get_retail_agent()
 
-    user = HumanUser()
-    task = TASKS_DEV[0]
+    task = TASKS_DEV[task_idx]
     instruction = task.instruction
+    user = SimulatedUser(instruction)
+    if user_type == "human":
+        user = HumanUser()
+    elif user_type == "simulated":
+        user = SimulatedUser(instruction)
+    else:
+        raise ValueError(f"Invalid user type: {user_type}")
+
     last_agent_response = ""
 
     gt_env = BaseEnv("retail")
@@ -29,12 +40,13 @@ def main():
     for i in range(max_steps):
         if i == 0:
             user_input = user.reset(instruction)
-            cprint(f"(Step {i}) User: {user_input}", "grey")
         else:
             user_input = user.step(last_agent_response)
-            cprint(f"(Step {i}) User: {user_input}", "grey")
 
-        if user_input == "### STOP":
+        cprint(f"(Step {i}) 👤 User: ", "white", end="", attrs=["bold"])
+        cprint(f" {user_input}", "white")
+
+        if STOP_TOKEN in user_input:
             break
 
         # pass user input to agent
@@ -44,21 +56,23 @@ def main():
                 "content": user_input,
             }
         )
-        cprint(f"(Step {i}) Agent: {agent_response}", "grey")
-        last_agent_response = agent_response
+        cprint(f"(Step {i}) 🤖 Agent: ", "light_blue", end="", attrs=["bold"])
+        cprint(f" {agent_response}", "light_blue")
+        last_agent_response = agent_response.content
+        cprint("-" * 20, "grey")
 
-    # Evaluate if the agent has complete the task
+    # Evaluate if the agent has complete the tasks
     for gt_tool in task.tool_calls:
         gt_tools_map[gt_tool.tool_name].run_impl(**gt_tool.arguments)
 
     if gt_env == agent.env:
-        cprint("✓ Task Successfully Completed!", "cyan")
+        cprint(f"✓ Task Successfully completed in {i} steps", "cyan", attrs=["bold"])
     else:
-        cprint("✗ Task Failed!", "red")
+        cprint(f"✗ Task Failed in {i} steps", "red", attrs=["bold"])
 
     gt_env.reset()
     agent.env.reset()
 
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    fire.Fire(run_single_task)
--- tau-benchmark/users/users.py
+++ tau-benchmark/users/users.py
@@ -5,7 +5,13 @@
 # the root directory of this source tree.
 
 import abc
+import uuid
 from typing import Optional
+
+from llama_stack_client import LlamaStackClient
+from llama_stack_client.lib.agents.agent import Agent
+from llama_stack_client.types.agent_create_params import AgentConfig
+from termcolor import cprint
 
 
 class BaseUser(abc.ABC):
@@ -20,66 +26,72 @@ class BaseUser(abc.ABC):
 
 class HumanUser(BaseUser):
     def reset(self, instruction: str) -> str:
-        return input(f"Initializing User...\n{instruction}\n> ")
+        cprint(f"Resetting User...\n{instruction}", "grey")
+        return input("\n> ")
 
     def step(self, content: str) -> str:
         return input("> ")
 
 
-# class SimulatedUser(BaseUser):
-#     def reset(self, instruction: str) -> str:
-#         return input(f"Initializing User...\n{instruction}\n> ")
+class SimulatedUser(BaseUser):
+    def __init__(self, instruction: str) -> None:
+        self.client = LlamaStackClient(
+            base_url="http://localhost:5000",
+        )
+        system_prompt = self.build_system_prompt(instruction)
+        self.agent_config = AgentConfig(
+            model="meta-llama/Llama-3.1-405B-Instruct-FP8",
+            instructions=system_prompt,
+            tools=[],
+            sampling_params={
+                "strategy": "greedy",
+                "temperature": 1.0,
+                "top_p": 0.9,
+            },
+            tool_choice="auto",
+            tool_prompt_format="json",
+            input_shields=[],
+            output_shields=[],
+            enable_session_persistence=False,
+        )
+        self.agent = Agent(self.client, self.agent_config)
+        self.session_id = self.agent.create_session(f"test-session-{uuid.uuid4()}")
 
-#     def step(self, content: str) -> str:
-#         return input("> ")
+    def reset(self, instruction: str) -> str:
+        cprint(f"Resetting User...\n{instruction}\n", "grey")
+        self.agent_config["instructions"] = self.build_system_prompt(instruction)
+        self.agent = Agent(self.client, self.agent_config)
+        self.session_id = self.agent.create_session(f"test-session-{uuid.uuid4()}")
 
+        response = self.agent.create_turn(
+            messages=[
+                {"role": "user", "content": "Hi! How can I help you today?"},
+            ],
+            session_id=self.session_id,
+        )
+        chunks = [chunk for chunk in response]
+        return chunks[-1].event.payload.turn.output_message.content
 
-# class LLMUserSimulationEnv(BaseUserSimulationEnv):
-#     def __init__(self, model: str, provider: str) -> None:
-#         super().__init__()
-#         self.messages: List[Dict[str, Any]] = []
-#         self.model = model
-#         self.provider = provider
-#         self.total_cost = 0.0
-#         self.reset()
+    def step(self, content: str) -> str:
+        response = self.agent.create_turn(
+            messages=[{"role": "user", "content": content}],
+            session_id=self.session_id,
+        )
+        chunks = [chunk for chunk in response]
+        return chunks[-1].event.payload.turn.output_message.content
 
-#     def generate_next_message(self, messages: List[Dict[str, Any]]) -> str:
-#         res = completion(
-#             model=self.model, custom_llm_provider=self.provider, messages=messages
-#         )
-#         message = res.choices[0].message
-#         self.messages.append(message.model_dump())
-#         self.total_cost = res._hidden_params["response_cost"]
-#         return message.content
-
-#     def build_system_prompt(self, instruction: Optional[str]) -> str:
-#         instruction_display = (
-#             ("\n\nInstruction: " + instruction + "\n")
-#             if instruction is not None
-#             else ""
-#         )
-#         return f"""You are a user interacting with an agent.{instruction_display}
-# Rules:
-# - Just generate one line at a time to simulate the user's message.
-# - Do not give away all the instruction at once. Only provide the information that is necessary for the current step.
-# - Do not hallucinate information that is not provided in the instruction. For example, if the agent asks for the order id but it is not mentioned in the instruction, do not make up an order id, just say you do not remember or have it.
-# - If the instruction goal is satisified, generate '###STOP###' as a standalone message without anything else to end the conversation.
-# - Do not repeat the exact instruction in the conversation. Instead, use your own words to convey the same information.
-# - Try to make the conversation as natural as possible, and stick to the personalities in the instruction."""
-
-#     def reset(self, instruction: Optional[str] = None) -> str:
-#         self.messages = [
-#             {
-#                 "role": "system",
-#                 "content": self.build_system_prompt(instruction=instruction),
-#             },
-#             {"role": "user", "content": "Hi! How can I help you today?"},
-#         ]
-#         return self.generate_next_message(self.messages)
-
-#     def step(self, content: str) -> str:
-#         self.messages.append({"role": "user", "content": content})
-#         return self.generate_next_message(self.messages)
-
-#     def get_total_cost(self) -> float:
-#         return self.total_cost
+    def build_system_prompt(self, instruction: Optional[str]) -> str:
+        instruction_display = (
+            ("\n\nInstruction: " + instruction + "\n")
+            if instruction is not None
+            else ""
+        )
+        return f"""You are a user interacting with an agent. {instruction_display}
+        Rules:
+        - Just generate one line at a time to simulate the user's message.
+        - Do not give away all the instruction at once. Only provide the information that is necessary for the current step.
+        - Do not hallucinate information that is not provided in the instruction. For example, if the agent asks for the order id but it is not mentioned in the instruction, do not make up an order id, just say you do not remember or have it.
+        - If the instruction goal is satisified, generate '###STOP###' as a standalone message without anything else to end the conversation.
+        - Do not repeat the exact instruction in the conversation. Instead, use your own words to convey the same information.
+        - Try to make the conversation as natural as possible, and stick to the personalities in the instruction.
+        """
