391a70f71e1e17fc0b8e0ba74ea3ebcaf81c5f48
Jeff Tang <jeff.x.tang@gmail.com>
2025-01-27 11:48:53 -0800

    iOSCalendarAssistant url fix

--- examples/ios_calendar_assistant/iOSCalendarAssistant/ContentView.swift
+++ examples/ios_calendar_assistant/iOSCalendarAssistant/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
   @State private var showAlert = false
   @State private var alertMessage = ""
   
-  private let agent = RemoteAgents(url: URL(string: "http://127.0.0.1:8321")!)
+  private let agent = RemoteAgents(url: URL(string: "http://127.0.0.1:5000")!)
   @State var agentId = ""
   @State var agenticSystemSessionId = ""
 
