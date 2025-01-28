cd7e80ce1893f65e67098692b9ea49781d3791ec
Chester Hu <hcp199242@gmail.com>
2025-01-27 16:04:22 -0800

    Update the implementation dependency to v0.1.0

--- examples/android_app/app/build.gradle.kts
+++ examples/android_app/app/build.gradle.kts
@@ -75,10 +75,6 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.1")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.18.1")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.1")
-//  implementation("com.llama.llamastack:llama-stack-client-kotlin:0.0.58")
-  implementation(fileTree(mapOf(
-    "dir" to "libs",
-    "include" to listOf("*.aar", "*.jar"),
-  )))
+  implementation("com.llama.llamastack:llama-stack-client-kotlin:0.1.0")
   implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
 }
