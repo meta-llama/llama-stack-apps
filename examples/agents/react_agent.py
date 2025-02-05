a8ac2f539687cc0014c4850cb898ccfc38fe5620
Xi Yan <xiyan@meta.com>
2025-02-04 19:31:12 -0800

    refactor

--- examples/agents/react_agent.py
+++ examples/agents/react_agent.py
@@ -5,9 +5,8 @@
 # the root directory of this source tree.
 # import os
 import json
-import re
 import uuid
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Union
 
 import fire
 
@@ -25,130 +24,140 @@ from llama_stack_client.types.shared.user_message import UserMessage
 from llama_stack_client.types.tool_def_param import Parameter
 from rich.pretty import pprint
 
-REACT_PROMPT = """
+REACT_JSON_PROMPT = """
 You are an expert assistant who can solve any task using tool calls. You will be given a task to solve as best you can.
 To do so, you have been given access to the following tools: <<tool_names>>
-The way you use the tools is by specifying a json blob, ending with '<end_action>'.
-Specifically, this json should have an `action` key (name of the tool to use) and an `action_input` key (input to the tool).
 
-The $ACTION_JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. It should be formatted in json. Do not try to escape special characters. Here is the template of a valid $ACTION_JSON_BLOB:
+You must always respond in the following JSON format:
 {
-  "action": $TOOL_NAME,
-  "action_input": $INPUT
-}<end_action>
+    "thought": $THOUGHT_PROCESS,
+    "action": {
+        "tool_name": $TOOL_NAME,
+        "tool_params": $TOOL_PARAMS
+    },
+    "observation": $OBSERVATION,
+    "answer": $ANSWER
+}
 
-Make sure to have the $INPUT as a dictionary in the right format for the tool you are using, and do not put variable names as input if you can find the right values.
+Specifically, this json should have a `thought` key, a `action` key, and an `observation` key.
 
-You should ALWAYS use the following format:
+The `action` key should specify the $TOOL_NAME the name of the tool to use and the `tool_params` key should specify the parameters key as input to the tool.
 
-Thought: you should always think about one action to take. Then use the action as follows:
-Action:
-$ACTION_JSON_BLOB
-Observation: the result of the action
-... (this Thought/Action/Observation can repeat N times, you should take several steps when needed. The $ACTION_JSON_BLOB must only use a SINGLE action at a time.)
+Make sure to have the $TOOL_PARAMS as a dictionary in the right format for the tool you are using, and do not put variable names as input if you can find the right values.
+
+You should always think about one action to take, and have the `thought` key contain your thought process about this action.
+The `observation` key should contain the result of the action
+... (this Thought/Action/Observation can repeat N times, you should take several steps when needed. The action key must only use a SINGLE tool at a time.)
 
 You can use the result of the previous action as input for the next action.
 The observation will always be a string: it can represent a file, like "image_1.jpg".
 Then you can use it as input for the next action. You can do it for instance as follows:
 
-Observation: "image_1.jpg"
-
-Thought: I need to transform the image that I received in the previous observation to make it green.
-Action:
 {
-  "action": "image_transformer",
-  "action_input": {"image": "image_1.jpg"}
-}<end_action>
+    "observation": "image_1.jpg",
+    "thought": "I need to transform the image that I received in the previous observation to make it green.",
+    "action": {
+        "tool_name": "image_transformer",
+        "tool_params": {"image": "image_1.jpg"}
+    },
+    "answer": null
+}
 
-To provide the final answer to the task, use an action blob with "action": "final_answer" tool. It is the only way to complete the task, else you will be stuck on a loop. So your final output should look like this:
-Action:
+
+To provide the final answer to the task, use the `answer` key. It is the only way to complete the task, else you will be stuck on a loop. So your final output should look like this:
 {
-  "action": "final_answer",
-  "action_input": {"answer": "insert your final answer here"}
-}<end_action>
-
+    "observation": "your observation",
+    "thought": "you thought process",
+    "action": null,
+    "answer": "insert your final answer here"
+}
 
 Here are a few examples using notional tools:
 ---
 Task: "Generate an image of the oldest person in this document."
 
-Thought: I will proceed step by step and use the following tools: `document_qa` to find the oldest person in the document, then `image_generator` to generate an image according to the answer.
-Action:
 {
-  "action": "document_qa",
-  "action_input": {"document": "document.pdf", "question": "Who is the oldest person mentioned?"}
-}<end_action>
+    "thought": "I will proceed step by step and use the following tools: `document_qa` to find the oldest person in the document, then `image_generator` to generate an image according to the answer.",
+    "action": {
+        "tool_name": "document_qa",
+        "tool_params": {"document": "document.pdf", "question": "Who is the oldest person mentioned?"}
+    },
+    "observation": "The oldest person in the document is John Doe, a 55 year old lumberjack living in Newfoundland.",
+    "answer": null
+}
 
-Observation: "The oldest person in the document is John Doe, a 55 year old lumberjack living in Newfoundland."
-
-
-Thought: I will now generate an image showcasing the oldest person.
-Action:
 {
-  "action": "image_generator",
-  "action_input": {"prompt": "A portrait of John Doe, a 55-year-old man living in Canada."}
-}<end_action>
-Observation: "image.png"
+    "thought": "I will now generate an image showcasing the oldest person.",
+    "action": {
+        "tool_name": "image_generator",
+        "tool_params": {"prompt": "A portrait of John Doe, a 55-year-old man living in Canada."}
+    },
+    "observation": "image.png",
+    "answer": null
+}
 
-Thought: I will now return the generated image.
-Action:
 {
-  "action": "final_answer",
-  "action_input": "image.png"
-}<end_action>
+    "thought": "I will now return the generated image.",
+    "action": null,
+    "answer": "image.png"
+}
 
 ---
 Task: "What is the result of the following operation: 5 + 3 + 1294.678?"
 
-Thought: I will use python code evaluator to compute the result of the operation and then return the final answer using the `final_answer` tool
-Action:
 {
-    "action": "python_interpreter",
-    "action_input": {"code": "5 + 3 + 1294.678"}
-}<end_action>
-Observation: 1302.678
+    "thought": "I will use python code evaluator to compute the result of the operation and then return the final answer using the `final_answer` tool",
+    "action": {
+        "tool_name": "python_interpreter",
+        "tool_params": {"code": "5 + 3 + 1294.678"}
+    },
+    "observation": 1302.678,
+    "answer": null
+}
 
-Thought: Now that I know the result, I will now return it.
-Action:
 {
-  "action": "final_answer",
-  "action_input": "1302.678"
-}<end_action>
+    "thought": "Now that I know the result, I will now return it.",
+    "action": null,
+    "observation": null,
+    "answer": 1302.678
+}
 
 ---
 Task: "Which city has the highest population , Guangzhou or Shanghai?"
 
-Thought: I need to get the populations for both cities and compare them: I will use the tool `search` to get the population of both cities.
-Action:
 {
-    "action": "search",
-    "action_input": "Population Guangzhou"
-}<end_action>
-Observation: ['Guangzhou has a population of 15 million inhabitants as of 2021.']
-
-
-Thought: Now let's get the population of Shanghai using the tool 'search'.
-Action:
-{
-    "action": "search",
-    "action_input": "Population Shanghai"
+    "thought": "I need to get the populations for both cities and compare them: I will use the tool `search` to get the population of both cities.",
+    "action": {
+        "tool_name": "search",
+        "tool_params": {"query": "Population Guangzhou"}
+    },
+    "observation": ['Guangzhou has a population of 15 million inhabitants as of 2021.'],
+    "answer": null
 }
-Observation: '26 million (2019)'
 
-Thought: Now I know that Shanghai has a larger population. Let's return the result.
-Action:
 {
-  "action": "final_answer",
-  "action_input": "Shanghai"
-}<end_action>
+    "thought": "Now let's get the population of Shanghai using the tool 'search'.",
+    "action": {
+        "tool_name": "search",
+        "tool_params": {"query": "Population Shanghai"}
+    },
+    "observation": "26 million (2019)",
+    "answer": null
+}
 
+{
+    "thought": "Now I know that Shanghai has a larger population. Let's return the result.",
+    "action": null,
+    "observation": null,
+    "answer": "Shanghai"
+}
 
 Above example were using notional tools that might not exist for you. You only have access to these tools:
 <<tool_descriptions>>
 
 Here are the rules you should always follow to solve your task:
-1. ALWAYS provide a single 'Thought:' sequence, and a single 'Action:' sequence that ends with <end_action>, else you will fail.
-2. Always use the right arguments for the tools. Never use variable names in the 'action_input' field, use the value instead.
+1. ALWAYS answer in the JSON format with keys "observation", "thought", "action", "answer", else you will fail.
+2. Always use the right arguments for the tools. Never use variable names in the 'tool_params' field, use the value instead.
 3. Call a tool only when needed: do not call the search agent if you do not need information, try to solve the task yourself.
 4. Never re-do a tool call that you previously did with the exact same parameters.
 5. Observations will be provided to you, no need to generate them
@@ -156,71 +165,55 @@ Here are the rules you should always follow to solve your task:
 Now Begin! If you solve the task correctly, you will receive a reward of $1,000,000.
 """
 
+from pydantic import BaseModel
+
+
+class Action(BaseModel):
+    tool_name: str
+    tool_params: Dict[str, Any]
+
+
+class ReActOutput(BaseModel):
+    thought: str
+    action: Optional[Action] = None
+    observation: Optional[str] = None
+    answer: Optional[str] = None
+
 
 class ReActOutputParser(OutputParser):
-    def _maybe_extract_action(self, text: str) -> Optional[Tuple[str, Dict[str, Any]]]:
-        """
-        Extract action name and parameters from the text format:
-
-        Thought: <some_text>
-
-        Action:
-        {
-        "action": <action_name>,
-        "action_input": <action_params>
-        }<end_action>
-
-        Args:
-            text (str): Input text containing the action block
-
-        Returns:
-            Tuple[str, Dict[str, Any]]: Tuple of (action_name, action_parameters)
-
-        Raises:
-            ValueError: If the action block cannot be parsed or is missing required fields
-        """
-        try:
-            # Find the action block using regex
-            action_pattern = r'Action:\s*{\s*"action":\s*"([^"]+)",\s*"action_input":\s*({[^}]+})\s*}<end_action>'
-            match = re.search(action_pattern, text, re.DOTALL)
-
-            if not match:
-                raise ValueError("Could not find valid action block in text")
-
-            action_name = match.group(1)
-            action_params = json.loads(match.group(2))
-
-            return action_name, action_params
-        except (ValueError, json.JSONDecodeError) as e:
-            print(f"Error parsing action: {e}")
-            return None
-
     def parse(self, output_message: CompletionMessage) -> CompletionMessage:
-        text = str(output_message.content)
-        action = self._maybe_extract_action(text)
-        if action is None or action[0] == "final_answer":
+        response_text = str(output_message.content)
+        try:
+            response_json = json.loads(response_text)
+        except json.JSONDecodeError as e:
+            print(f"Error parsing action: {e}")
             return output_message
 
-        action_name, action_params = action
-        call_id = str(uuid.uuid4())
-        output_message.tool_calls = [
-            ToolCall(
-                call_id=call_id,
-                tool_name=action_name,
-                arguments=action_params,
-            )
-        ]
+        if response_json.get("answer", None):
+            return output_message
+
+        if response_json.get("action", None):
+            tool_name = response_json["action"].get("tool_name", None)
+            tool_params = response_json["action"].get("tool_params", None)
+            if tool_name and tool_params:
+                call_id = str(uuid.uuid4())
+                output_message.tool_calls = [
+                    ToolCall(
+                        call_id=call_id, tool_name=tool_name, arguments=tool_params
+                    )
+                ]
+
         return output_message
 
 
-class MetaExternalSearchTool(ClientTool):
+class SearchTool(ClientTool):
 
     def get_name(self) -> str:
-        return "get_external_meta_data"
+        return "search"
 
     def get_description(self) -> str:
         return """
-        Search the web for the given query about Meta. Get information Meta available on the public internet
+        Search the web for the given query.
         """
 
     def get_params_definition(self) -> Dict[str, Parameter]:
@@ -236,6 +229,9 @@ class MetaExternalSearchTool(ClientTool):
     def run(
         self, messages: List[Union[UserMessage, ToolResponseMessage]]
     ) -> List[Union[UserMessage, ToolResponseMessage]]:
+        from rich.pretty import pprint
+
+        pprint(messages)
         print("run for MetaExternalSearchTool called")
         dummy_response = """
             torchtune is a PyTorch library for easily authoring, finetuning and experimenting with LLMs.
@@ -263,17 +259,17 @@ def main():
         base_url="http://localhost:8321",
     )
 
-    model = "meta-llama/Llama-3.3-70B-Instruct"
+    model = "meta-llama/Llama-3.1-8B-Instruct"
 
     client_tools = [
-        MetaExternalSearchTool(),
+        SearchTool(),
     ]
 
     tool_names = ", ".join([tool.get_name() for tool in client_tools])
     tool_descriptions = "\n".join(
         [f"- {tool.get_name()}: {tool.get_description()}" for tool in client_tools]
     )
-    instruction = REACT_PROMPT.replace("<<tool_names>>", tool_names).replace(
+    instruction = REACT_JSON_PROMPT.replace("<<tool_names>>", tool_names).replace(
         "<<tool_descriptions>>", tool_descriptions
     )
 
@@ -287,11 +283,16 @@ def main():
             client_tool.get_tool_definition() for client_tool in client_tools
         ],
         tool_choice="auto",
-        tool_prompt_format="python_list",
+        tool_prompt_format="json",
         input_shields=[],
         output_shields=[],
         enable_session_persistence=False,
+        # response_format={
+        #     "type": "json_schema",
+        #     "json_schema": ReActOutput.model_json_schema(),
+        # },
     )
+
     agent = Agent(client, agent_config, client_tools, output_parser=ReActOutputParser())
 
     session_id = agent.create_session(f"ttest-session-{uuid.uuid4().hex}")
