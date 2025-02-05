c205e6a138bb3ea96b60d77abca9100c05f3d401
Xi Yan <xiyan@meta.com>
2025-02-05 10:31:06 -0800

    use sdk

--- examples/agents/react_agent.py
+++ examples/agents/react_agent.py
@@ -14,6 +14,9 @@ from llama_stack_client import LlamaStackClient
 from llama_stack_client.lib.agents.agent import Agent
 from llama_stack_client.lib.agents.client_tool import ClientTool
 from llama_stack_client.lib.agents.output_parser import OutputParser
+from llama_stack_client.lib.agents.react.prompts import (
+    DEFAULT_REACT_AGENT_SYSTEM_PROMPT_TEMPLATE,
+)
 
 # from llama_stack_client.lib.agents.event_logger import EventLogger
 from llama_stack_client.types.agent_create_params import AgentConfig
@@ -22,150 +25,9 @@ from llama_stack_client.types.shared.tool_call import ToolCall
 from llama_stack_client.types.shared.tool_response_message import ToolResponseMessage
 from llama_stack_client.types.shared.user_message import UserMessage
 from llama_stack_client.types.tool_def_param import Parameter
-from rich.pretty import pprint
-
-REACT_JSON_PROMPT = """
-You are an expert assistant who can solve any task using tool calls. You will be given a task to solve as best you can.
-To do so, you have been given access to the following tools: <<tool_names>>
-
-You must always respond in the following JSON format:
-{
-    "thought": $THOUGHT_PROCESS,
-    "action": {
-        "tool_name": $TOOL_NAME,
-        "tool_params": $TOOL_PARAMS
-    },
-    "observation": $OBSERVATION,
-    "answer": $ANSWER
-}
-
-Specifically, this json should have a `thought` key, a `action` key, and an `observation` key.
-
-The `action` key should specify the $TOOL_NAME the name of the tool to use and the `tool_params` key should specify the parameters key as input to the tool.
-
-Make sure to have the $TOOL_PARAMS as a dictionary in the right format for the tool you are using, and do not put variable names as input if you can find the right values.
-
-You should always think about one action to take, and have the `thought` key contain your thought process about this action.
-The `observation` key should contain the result of the action
-... (this Thought/Action/Observation can repeat N times, you should take several steps when needed. The action key must only use a SINGLE tool at a time.)
-
-You can use the result of the previous action as input for the next action.
-The observation will always be a string: it can represent a file, like "image_1.jpg".
-Then you can use it as input for the next action. You can do it for instance as follows:
-
-{
-    "observation": "image_1.jpg",
-    "thought": "I need to transform the image that I received in the previous observation to make it green.",
-    "action": {
-        "tool_name": "image_transformer",
-        "tool_params": {"image": "image_1.jpg"}
-    },
-    "answer": null
-}
-
-
-To provide the final answer to the task, use the `answer` key. It is the only way to complete the task, else you will be stuck on a loop. So your final output should look like this:
-{
-    "observation": "your observation",
-    "thought": "you thought process",
-    "action": null,
-    "answer": "insert your final answer here"
-}
-
-Here are a few examples using notional tools:
----
-Task: "Generate an image of the oldest person in this document."
-
-{
-    "thought": "I will proceed step by step and use the following tools: `document_qa` to find the oldest person in the document, then `image_generator` to generate an image according to the answer.",
-    "action": {
-        "tool_name": "document_qa",
-        "tool_params": {"document": "document.pdf", "question": "Who is the oldest person mentioned?"}
-    },
-    "observation": "The oldest person in the document is John Doe, a 55 year old lumberjack living in Newfoundland.",
-    "answer": null
-}
-
-{
-    "thought": "I will now generate an image showcasing the oldest person.",
-    "action": {
-        "tool_name": "image_generator",
-        "tool_params": {"prompt": "A portrait of John Doe, a 55-year-old man living in Canada."}
-    },
-    "observation": "image.png",
-    "answer": null
-}
-
-{
-    "thought": "I will now return the generated image.",
-    "action": null,
-    "answer": "image.png"
-}
-
----
-Task: "What is the result of the following operation: 5 + 3 + 1294.678?"
-
-{
-    "thought": "I will use python code evaluator to compute the result of the operation and then return the final answer using the `final_answer` tool",
-    "action": {
-        "tool_name": "python_interpreter",
-        "tool_params": {"code": "5 + 3 + 1294.678"}
-    },
-    "observation": 1302.678,
-    "answer": null
-}
-
-{
-    "thought": "Now that I know the result, I will now return it.",
-    "action": null,
-    "observation": null,
-    "answer": 1302.678
-}
-
----
-Task: "Which city has the highest population , Guangzhou or Shanghai?"
-
-{
-    "thought": "I need to get the populations for both cities and compare them: I will use the tool `search` to get the population of both cities.",
-    "action": {
-        "tool_name": "search",
-        "tool_params": {"query": "Population Guangzhou"}
-    },
-    "observation": ['Guangzhou has a population of 15 million inhabitants as of 2021.'],
-    "answer": null
-}
-
-{
-    "thought": "Now let's get the population of Shanghai using the tool 'search'.",
-    "action": {
-        "tool_name": "search",
-        "tool_params": {"query": "Population Shanghai"}
-    },
-    "observation": "26 million (2019)",
-    "answer": null
-}
-
-{
-    "thought": "Now I know that Shanghai has a larger population. Let's return the result.",
-    "action": null,
-    "observation": null,
-    "answer": "Shanghai"
-}
-
-Above example were using notional tools that might not exist for you. You only have access to these tools:
-<<tool_descriptions>>
-
-Here are the rules you should always follow to solve your task:
-1. ALWAYS answer in the JSON format with keys "observation", "thought", "action", "answer", else you will fail.
-2. Always use the right arguments for the tools. Never use variable names in the 'tool_params' field, use the value instead.
-3. Call a tool only when needed: do not call the search agent if you do not need information, try to solve the task yourself.
-4. Never re-do a tool call that you previously did with the exact same parameters.
-5. Observations will be provided to you, no need to generate them
-
-Now Begin! If you solve the task correctly, you will receive a reward of $1,000,000.
-"""
 
 from pydantic import BaseModel
+from rich.pretty import pprint
 
 
 class Action(BaseModel):
@@ -176,7 +38,6 @@ class Action(BaseModel):
 class ReActOutput(BaseModel):
     thought: str
     action: Optional[Action] = None
-    observation: Optional[str] = None
     answer: Optional[str] = None
 
 
@@ -269,9 +130,9 @@ def main():
     tool_descriptions = "\n".join(
         [f"- {tool.get_name()}: {tool.get_description()}" for tool in client_tools]
     )
-    instruction = REACT_JSON_PROMPT.replace("<<tool_names>>", tool_names).replace(
-        "<<tool_descriptions>>", tool_descriptions
-    )
+    instruction = DEFAULT_REACT_AGENT_SYSTEM_PROMPT_TEMPLATE.replace(
+        "<<tool_names>>", tool_names
+    ).replace("<<tool_descriptions>>", tool_descriptions)
 
     agent_config = AgentConfig(
         model=model,
