994d8b47c53d6627f1b42d37a2d90d5cd505b54c
Kai Wu <wukaixingxp@gmail.com>
2025-02-26 19:07:09 -0800

    Make DocQA a one-clickable app implementation (#151)
    # What does this PR do?
    This PR adds a one-clickable app implementation of DocQA example, where
    user can choose local provider like Ollama or cloud provider like
    together or fireworks to do RAG. It allows users to install the `DocQA`
    app into the Application folder from the `DocQA.dmg` file. This `MacQA`
    app leverages llama-stack new `LlamaStackAsLibraryClient` feature and
    make every agentic component in-line. This PR also explains the steps
    for building this app.
    
    This DocQA app will replace the existing docker solution as the docker
    solution is not easy to user and maintain.
    
    ## Before submitting
    - [ ] This PR fixes a typo or improves the docs (you can dismiss the
    other checks if that's the case).
    - [x] Did you read the [contributor
    guideline](https://github.com/meta-llama/llama-stack-apps/blob/main/CONTRIBUTING.md#pull-requests),
          Pull Request section?
    - [ ] Was this discussed/approved via a Github issue? Please add a link
          to it if that's the case.
    - [x] Did you make sure to update the documentation with your changes?
    - [x] Did you write any new necessary tests?
    
    Thanks for contributing 🎉!
    
    ---------
    
    Co-authored-by: Hamid Shojanazeri <hamid.nazeri2010@gmail.com>

--- examples/DocQA/docker/ollama_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "-------------start to serve------------"
-OLLAMA_HOST=127.0.0.1:14343 /usr/bin/ollama  serve &
-echo "Running ollama model: $MODEL_NAME"
-sleep 5
-OLLAMA_HOST=127.0.0.1:14343 /usr/bin/ollama run $MODEL_NAME
